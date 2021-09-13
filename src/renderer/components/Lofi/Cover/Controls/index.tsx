3d94c0afc1e2225f30f742d4891db164f906cfdc
Nicolas St-Amour <stamoun@gmail.com>
2021-09-13 10:10:11 -0400

    Merge from master

--- src/renderer/components/Lofi/Cover/Controls/index.tsx
+++ src/renderer/components/Lofi/Cover/Controls/index.tsx
@@ -138,6 +138,10 @@ class Controls extends React.Component<any, any> {
                       className="control-btn secondary-control not-draggable skip">
                       <i className="fa fa-step-forward not-draggable"></i>
                     </a>
+              </p>
+            ) : null}
+            {this.props.parent.isTrackLiked() !== null ? (
+              <p className="row">
                     <a
                       onClick={this.like.bind(this)}
                       className="love-control-btn tertiary-control not-draggable">
